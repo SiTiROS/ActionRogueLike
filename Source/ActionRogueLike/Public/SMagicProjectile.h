4370074275fbaa1b80624e0ab0ae925da9745698
SiTiROS <sitiros666@gmail.com>
2024-11-14 03:03:18 +0600

    16 Projectile Blueprint & Collision

--- Source/ActionRogueLike/Public/SMagicProjectile.h
+++ Source/ActionRogueLike/Public/SMagicProjectile.h
@@ -16,13 +16,13 @@ public:
 	ASMagicProjectile();
 
 protected:
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	USphereComponent* SphereComp;
 
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	UProjectileMovementComponent* MovementComp;
 
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	UParticleSystemComponent* EffectComp;
 
 	virtual void BeginPlay() override;
