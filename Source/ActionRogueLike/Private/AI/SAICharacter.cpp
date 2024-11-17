1edc0eb64a3f610b5bb702c86b2db2c6098e50ca
SiTiROS <sitiros666@gmail.com>
2024-11-18 02:30:58 +0600

    40 Bot Behaviors and Movement

---
+++ Source/ActionRogueLike/Private/AI/SAICharacter.cpp
@@ -0,0 +1,19 @@
+#include "AI/SAICharacter.h"
+
+ASAICharacter::ASAICharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ASAICharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASAICharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
